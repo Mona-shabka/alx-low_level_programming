4bc9edea12b71ed3155c0f81438db12059fc872f
mona-shabka <mona.shabka@yahoo.com>
2023-08-17 06:35:36 -0700

    1

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,24 @@
-#
+#include "main.h"
+
+/**
+* print_line - a function that draws
+*               a straight line in the terminal.
+* @n: input for number of _ character.
+*/
+
+void print_line(int n)
+{
+	int m;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (m = 1; m <= n; m++)
+			_putchar('\');
+
+		_putchar('\n');
+	}
+}
