f2154f3f5faf33bb1f43e3f628b316968497ecb0
mona-shabka <mona.shabka@yahoo.com>
2023-08-17 07:28:42 -0700

    1

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_square - a function that draws
+*		a square in the terminal.
+* @size: input for number of # character.
+*/
+
+void print_square(int size)
+{
+	int n, s;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (n = 1; n <= size; n++)
+		{
+			_putchar('#');
+			for (s = 2; s <= n; s++)
+				_putchar('#');
+		_putchar('\n');
+		}
+	}
+}
