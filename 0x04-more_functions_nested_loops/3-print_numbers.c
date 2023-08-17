d235aebe0c674a777f13bfd767f487d2ec6c77fb
mona-shabka <mona.shabka@yahoo.com>
2023-08-17 03:11:27 -0700

    1

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,7 +4,7 @@
 * print_numbers - a function that prints the numbers,
 *			from 0 to 9, followed by a new line.
 * Description: print numbers 0 to 9.
-* Return: Always 0 (success).
+* Return: void
 */
 
 void print_numbers(void)
