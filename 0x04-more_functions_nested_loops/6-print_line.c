7d76113f149aa11ba97bdf6a68ec7da989519fbf
mona-shabka <mona.shabka@yahoo.com>
2023-08-17 06:06:33 -0700

    1

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,7 @@
 /**
 * print_line - a function that draws
 *		a straight line in the terminal.
-* @n: input for line.
+* @n: input for number of _ character.
 * Return: 0 (success).
 */
 
@@ -11,12 +11,11 @@ void print_line(int n)
 {
 	for (n = 1; n >= 1; n++)
 	{
-		_putchar(95);
-
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
+		_putchar('_');
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
 		_putchar('\n');
 	}
 }
