8c38be5ea89ec3f6b625d6820aabe6c9536acd6d
mona-shabka <mona.shabka@yahoo.com>
2023-08-17 03:38:43 -0700

    1

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,19 +5,17 @@
 *			from 0 to 9,
 *			followed by a new line.
 * Description: print numbers 0 to 9.
-* Return: void.
+* Return: 0 (success)
 */
 
 void print_numbers(void)
 {
 	int n = 0;
 
-	if (n >= 0 && n <= 9)
+	for (n >= 0; n <= 9;)
 	{
 		_putchar(n + 48);
 		n++;
 	}
-
 	_putchar('\n');
-
 }
