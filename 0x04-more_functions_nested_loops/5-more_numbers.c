956bb4e0bb01d0fde3318325dee17c849fd14080
mona-shabka <mona.shabka@yahoo.com>
2023-08-17 02:32:07 -0700

    1

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* more_numbers - a function that prints 10 times the numbers,
+*			from 0 to 14, followed by a new line.
+* Return: Always 0 (success).
+*/
+
+void more_numbers(void)
+{
+	int c;
+
+	for (int n = 0; n >= 10; n++)
+	{
+		if (c >= 0 && c <= 14)
+		{
+			putchar(c + 48);
+			c++;
+		}
+		putchar('\n');
+	}
+	return (0);
+}
