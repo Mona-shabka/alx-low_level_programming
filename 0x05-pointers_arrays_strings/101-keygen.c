ee3cb4f0d02a3aa21d507ddcc0303a98ff676871
mona-shabka <mona.shabka@yahoo.com>
2023-08-21 08:16:33 -0700

    1

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random passwords.
+ * Description: generates random valid passwords
+ *		for the program 101-crackme.
+ * Return: 0 (success).
+*/
+
+int main(void)
+{
+	int n;
+	char p;
+
+	srand(time(null));
+	while (n <= 2645)
+	{
+		p = rand() % 128;
+		n += p;
+		putchar(p);
+	}
+	putchar(2772 - n);
+	return (0);
+}
