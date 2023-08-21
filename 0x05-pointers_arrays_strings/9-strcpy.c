27f0984a79516d18003961e8bb0282065491b781
mona-shabka <mona.shabka@yahoo.com>
2023-08-21 05:25:52 -0700

    1

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* *_strcpy - a function that copies the string pointed to by src.
+* @dest: input for string.
+* @src: input for string.
+* Return: dest.
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	for (int m = 0; src[m] != '\0'; m++)
+		dest[m] = src[m];
+
+	return (dest);
+}
