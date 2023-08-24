54595d9b1134cbe9820c4c30c0adc4e29689be1e
mona-shabka <mona.shabka@yahoo.com>
2023-08-24 03:13:25 -0700

    1

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,34 @@
 #include "main.h"
 
 /**
+ * isLower - afunction that show lowercase ascii.
+ * @m: input of letters.
+ * Return: 1(true) 0 (false).
+*/
+
+int isLower(char m)
+{
+	return (m >= 97 && m <= 122);
+}
+
+/**
+ * isDelimiter - a function that show the delimiter ascii.
+ * @m: input of letters.
+ * Return: 1 (true) and 0 (false).
+*/
+
+int isDelimiter(char m)
+{
+	int i = 0;
+	char d[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (m == d[i])
+			return (1);
+	return (0);
+}
+
+/**
 * cap_string - a function that capitalizes all words of a string.
 * @str: input for string.
 * Return: string of capital.
@@ -8,4 +36,21 @@
 
 char *cap_string(char *str)
 {
+	char *p = str;
+	int l = 1;
 
+	while (*str)
+	{
+		if (isDelimiter(*str))
+			l = 1;
+		else if (isLower(*str) && l)
+		{
+			*str -= 32;
+			l = 0;
+		}
+		else
+			l = 0;
+		str++;
+	}
+	return (p);
+}
