a7bb59617ebd2b67ce1f0f67139968ae04cf1d5f
mona-shabka <mona.shabka@yahoo.com>
2023-09-07 06:03:17 -0700

    1

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* memoryset - a function that enter memory by constant.
+* @str: pointer.
+* @a: constant.
+* @b: number of bytes that used.
+* Return: pointer (str).
+*/
+
+char *memoryset(char *str, char a, unsigned int b)
+{
+	char *point = str;
+
+	while (--b)
+		*str++ = a;
+	return (point);
+}
+
+/**
+* _calloc - a function that allocates memory for an array, using malloc.
+* @nmemb: array.
+* @size: size element.
+* Return: void.
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m_array;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	m_array = malloc(nmemb * sizeof(int));
+
+	if (m_array == 0)
+		return (NULL);
+
+	memoryset(m_array, 0, sizeof(int) * nmemb);
+	return (m_array);
+}
