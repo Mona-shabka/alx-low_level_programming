aa792f466a883d76606a5d682952cc23623648b7
mona-shabka <mona.shabka@yahoo.com>
2023-09-07 13:12:28 -0700

    1

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
 * _isdigit - a function that checks for numbers in string.
 * @m: input for string.
 * Return: 0 (non-digit) 1 (number).
 */
-
 int _isdigit(char *m)
 {
 	int n = 0;
@@ -18,13 +16,11 @@ int _isdigit(char *m)
 	}
 	return (1);
 }
-
 /**
 * _strlen - a function that calculate the length of string.
 * @m: input string.
 * Return: int number of string length.
 */
-
 int _strlen(char *m)
 {
 	int n = 0;
@@ -35,30 +31,26 @@ int _strlen(char *m)
 	}
 	return (n);
 }
-
 /**
- * _error - a function that check the error of string.
- */
-
+* _error - a function that check the error of string.
+*/
 void _error(void)
 {
 	printf("Error\n");
 	exit(98);
 }
-
 /**
 * main - a function that that multiplies two positive numbers.
 * @argc:argument numbers.
 * @argv: argument string.
 * Return: 0 means success.
 */
-
 int main(int argc, char *argv[])
 {
 	char *m1, *m2;
 	int length1, length2, length, i, carry, num1, num2, *result, a = 0;
-	m1 = argv[1];
-	m2 = argv[2];
+
+	m1 = argv[1], m2 = argv[2];
 	if (argc != 3 || !_isdigit(m1) || !_isdigit(m2))
 		_error();
 	length1 = _strlen(m1);
