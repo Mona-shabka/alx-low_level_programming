658eb2ee88930e7002b31a420605c097afc07850
mona-shabka <mona.shabka@yahoo.com>
2023-09-07 13:03:00 -0700

    1

---
+++ 0x0C-more_malloc_free/101-m.c
@@ -0,0 +1,94 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+* _puts - a function that print string.
+* @m: string pointer.
+* Return: void.
+*/
+
+void _puts(char *m)
+{
+	int n = 0;
+
+	while (m[n])
+	{
+		_putchar(m[n]);
+		n++;
+	}
+}
+
+/**
+* _atoi - a function that change charachter to number.
+* @mon: string.
+* Return: numbers.
+*/
+
+int _atoi(const char *mon)
+{
+	int sign = 1;
+	unsigned long int r = 0, num, n;
+
+	for (num = 0; !(mon[num] >= '0' && mon[num] <= '9'); num++)
+	{
+		if (mon[num] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (n = num; mon[n] >= '0' && mon[n] <= '9'; n++)
+	{
+		r *= 10;
+		r += (mon[n] - '0');
+	}
+
+	return (sign * r);
+}
+
+/**
+* print_int - a function that print numbers.
+* @m: number.
+* Return: 0
+*/
+
+void print_int(unsigned long int m)
+{
+	unsigned long int d = 1, i, r;
+
+	for (i = 0; m / d > 9; i++, d *= 10)
+	{
+		;
+	}
+
+	for (; d >= 1; m %= d, d /= 10)
+	{
+		r = m / d;
+		_putchar('0' + r);
+	}
+}
+
+/**
+* main - a function that reallocates a memory block
+*		using malloc and free.
+* @argc: number of arguments.
+* @argv: array list.
+* Return: 0.
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	if (argc > 3)
+	{
+		_puts("Error\n");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
+
+	return (0);
+}
--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,95 +1,98 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-#define ERR_MSG "Error"
 
 /**
-* _puts - a function that print string.
-* @m: string pointer.
-* Return: void.
+* _isdigit - a function that checks for numbers in string.
+* @m: input for string.
+* Return: 0 (non-digit) 1 (number).
 */
 
-void _puts(char *m)
+int _isdigit(char *m)
 {
 	int n = 0;
 
 	while (m[n])
 	{
-		_putchar(m[n]);
+		if (m[n] < '0' || m[n] > '9')
+			return (0);
 		n++;
 	}
+	return (1);
 }
 
 /**
-* _atoi - a function that change charachter to number.
-* @mon: string.
-* Return: numbers.
+* _strlen - a function that calculate the length of string.
+* @m: input string.
+* Return: int number of string length.
 */
 
-int _atoi(const char *mon)
+int _strlen(char *m)
 {
-	int sign = 1;
-	unsigned long int r = 0, num, n;
+	int n = 0;
 
-	for (num = 0; !(mon[num] >= '0' && mon[num] <= '9'); num++)
+	while (m[n] != '\0')
 	{
-		if (mon[num] == '-')
+		n++;
+	}
+	return (n);
+}
+
+/**
+ * _error - a function that check the error of string.
+ */
+
+void _error(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+* main - a function that that multiplies two positive numbers.
+* @argc:argument numbers.
+* @argv: argument string.
+* Return: 0 means success.
+*/
+
+int main(int argc, char *argv[])
+{
+	char *m1, *m2;
+	int length1, length2, length, i, carry, num1, num2, *result, a = 0;
+	m1 = argv[1];
+	m2 = argv[2];
+	if (argc != 3 || !_isdigit(m1) || !_isdigit(m2))
+		_error();
+	length1 = _strlen(m1);
+	length2 = _strlen(m2);
+	length = length1 + length2 + 1;
+	result = malloc(sizeof(int) * length);
+	if (!result)
+		return (1);
+	for (i = 0; i <= length1 + length2; i++)
+		result[i] = 0;
+	for (length1 = length1 - 1; length1 >= 0; length1--)
+	{
+		num1 = m1[length1] - '0';
+		carry = 0;
+		for (length2 = _strlen(m2) - 1; length2 >= 0; length2--)
 		{
-			sign *= -1;
+			num2 = m2[length2] - '0';
+			carry += result[length1 + length2 + 1] + (num1 * num2);
+			result[length1 + length2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[length1 + length2 + 1] += carry;
 	}
-
-	for (n = num; mon[n] >= '0' && mon[n] <= '9'; n++)
+	for (i = 0; i < length - 1; i++)
 	{
-		r *= 10;
-		r += (mon[n] - '0');
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-
-	return (sign * r);
-}
-
-/**
-* print_int - a function that print numbers.
-* @m: number.
-* Return: 0
-*/
-
-void print_int(unsigned long int m)
-{
-	unsigned long int d = 1, i, r;
-
-	for (i = 0; m / d > 9; i++, d *= 10)
-	{
-		;
-	}
-
-	for (; d >= 1; m %= d, d /= 10)
-	{
-		r = m / d;
-		_putchar('0' + r);
-	}
-}
-
-/**
-* main - a function that reallocates a memory block
-*		using malloc and free.
-* @argc: number of arguments.
-* @argv: array list.
-* Return: 0.
-*/
-
-int main(int argc, char const *argv[])
-{
-	(void)argc;
-
-	if (argc > 3)
-	{
-		_puts("Error\n");
-		exit(98);
-	}
-	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
-
+	free(result);
 	return (0);
 }
