8318c452479f309e231d28b6f13dcba9b3004d3f
mona-shabka <mona.shabka@yahoo.com>
2023-09-11 06:45:06 -0700

    1

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
-* strlen - a function that return length of string.
+* _strlen - a function that return length of string.
 * @mon: input string.
 * Return: length of string.
 */
 
-int strlen(const char *mon)
+int _strlen(const char *mon)
 {
 	int length = 0;
 
@@ -17,13 +17,13 @@ int strlen(const char *mon)
 }
 
 /**
-* strcopy - a function that prints string to another string.
+* _strcopy - a function that prints string to another string.
 * @new: new string that receive string.
 * @mon: string that copy happen from it.
 * Return: new string.
 */
 
-char *strcopy(char *new, char *mon)
+char *_strcopy(char *new, char *mon)
 {
 	int length;
 
@@ -50,21 +50,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = (dog_t *) malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	dog->name = strcopy(dog->name, name);
+	dog->name = _strcopy(dog->name, name);
 	dog->age = age;
-	dog->owner = strcopy(dog->owner, owner);
+	dog->owner = _strcopy(dog->owner, owner);
 	return (dog);
 }
