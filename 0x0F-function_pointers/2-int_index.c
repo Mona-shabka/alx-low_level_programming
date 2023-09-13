362c0d3f674eaf5236042092c2ff5ffb2b31c851
mona-shabka <mona.shabka@yahoo.com>
2023-09-13 04:30:16 -0700

    1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,11 +12,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int length;
 
-	if (size <= 0 && !array && !cmp)
+	if (size <= 0 || !array || !cmp)
 	{
 		return (-1);
 	}
-	else
+	if (size && array && cmp)
 	{
 		for (length = 0; length < size; length++)
 		{
