8d4cae6c118b5684636b3f33d1ed06f548cd63bb
mona-shabka <mona.shabka@yahoo.com>
2023-09-13 05:53:24 -0700

    1

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,15 +12,19 @@ int main(int argc, char **argv)
 	int x, y;
 	int (*func)(int, int);
 
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+
 	x = atoi(argv[1]);
 	y = atoi(argv[3]);
-	if (argc == 4)
-		printf("Error\n"), exit(98);
+
 	func = get_op_func(argv[2]);
 	if (!func)
 		printf("Error\n"), exit(99);
+
 	if (!y && (argv[2][0] == '/' || argv[2][0] == '%'))
 		printf("Error\n"), exit(100);
+
 	printf("%d\n", func(x, y));
 	return (0);
 }
