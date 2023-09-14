9d3dafea3e46791bed62ff930e8a6f777e4d2f66
mona-shabka <mona.shabka@yahoo.com>
2023-09-14 06:31:37 -0700

    1

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,88 @@
+#include "variadic_functions.h"
+
+/**
+* format_char - a program that check the format of character.
+* @separator: separator is the string to be printed between the strings.
+* @listt: pointer of argument.
+* Return: void
+*/
+
+void format_char(char *separator, va_list listt)
+{
+	printf("%s%c", separator, va_arg(listt, int));
+}
+
+/**
+* format_int - a program that check the format of integers numbers.
+* @separator: separator is the string to be printed between the strings.
+* @listt: pointer of argument.
+* Return: void
+*/
+
+void format_int(char *separator, va_list listt)
+{
+	printf("%s%d", separator, va_arg(listt, int));
+}
+
+/**
+* format_float - a program that check the format of float numbers.
+* @separator: separator is the string to be printed between the strings.
+* @listt: pointer of argument.
+* Return: void
+*/
+
+void format_float(char *separator, va_list listt)
+{
+	printf("%s%f", separator, va_arg(listt, double));
+}
+
+/**
+* format_string - a program that check the format of strings.
+* @separator: separator is the string to be printed between the strings.
+* @listt: pointer of argument.
+* Return: void
+*/
+
+void format_string(char *separator, va_list listt)
+{
+	char *string = va_arg(listt, char *);
+
+	printf("%s%s", separator, string ? string : "(nil)");
+}
+
+/**
+* print_all -  a function that prints anything.
+* @format: format of string.
+* Return: void.
+*/
+
+void print_all(const char * const format, ...)
+{
+	int x = 0, y;
+	char *separator = "";
+	va_list listt;
+	list_l tokens[] = {
+		{"c", format_char},
+		{"i", format_int},
+		{"f", format_float},
+		{"s", format_string},
+		{NULL, NULL}
+	};
+	va_start(listt, format);
+	while (format && format[x])
+	{
+		y = 0;
+		while (tokens[y].list)
+		{
+			if (format[x] == tokens[y].list[0])
+			{
+				tokens[y].fun(separator, listt);
+				separator = ", ";
+			}
+			y++;
+		}
+		x++;
+	}
+	printf("\n");
+	va_end(listt);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,18 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct list - struction of list.
+ * @chara: format of list.
+ * @fun: function that considered in process.
+*/
+
+typedef struct list
+{
+	char *chara;
+	void (*fun)(char *, va_list);
+} list_l;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
