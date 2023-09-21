d8709b8570ab2b3c1c1c86e21dfc4663d8a6a06b
mona-shabka <mona.shabka@yahoo.com>
2023-09-20 21:40:25 -0700

    1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * _strlen - a function that give the length of string.
+ * @m: input string.
+ * Return: string length.
+ */
+
+int _strlen(char *m)
+{
+	int n = 0;
+
+	if (!m)
+		return (0);
+	while (*m++)
+		n++;
+	return (n);
+}
+
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the elements of a list_t list.
+ * @h: first node pointer.
+ * Return: list size.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		printf("[%d] %s/n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
