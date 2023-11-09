c0c54dad2b341da946655a36e8b159995382eaa2
Mona-shabka <mona.shabka@yahoo.com>
2023-11-09 05:41:46 -0800

    1

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - a function that frees a dlistint_t list.
+ * @head: head node pointer.
+ * Return: void.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *n;
+
+	while (head)
+	{
+		n = head;
+		head = head->next;
+		free(n)
+	}
+}
